e9941a93632eef3a125dd32b310250564eed12fe
anatoly.igumnov <anatoly.igumnov@gmail.com>
2023-09-18 16:56:32 +1100

    finale

--- src/ComboBox.java
+++ src/ComboBox.java
@@ -20,7 +20,15 @@ public class ComboBox extends JFrame {
         this.setLocation(d.width / 2 - ComboBox.width / 2, d.height / 2 - ComboBox.height / 2);
         this.getContentPane().setBackground(Color.orange);
         this.setLocationRelativeTo(null);
-        this.setLayout(new GridBagLayout());
+        this.setLayout(new BorderLayout());
+
+        JPanel panelUp = new JPanel(new GridBagLayout());
+        this.add(panelUp, BorderLayout.CENTER);
+
+        JPanel panelDown = new JPanel(new GridLayout());
+        panelDown.setPreferredSize(new Dimension(0, 70));
+        panelDown.setBackground(Color.white);
+        this.add(panelDown, BorderLayout.SOUTH);
 
         comboBox = new JComboBox<>(new String[] {"Красный","Желтый","Фиолетовый","Коричневый","Оранжевый"});
 
@@ -30,58 +38,46 @@ public class ComboBox extends JFrame {
         field.setEnabled(false);
 
         JButton button = new JButton("Ответить");
-        button.setFont(new Font("Serif", Font.PLAIN, 20));
 
         label = new JLabel("Ответ:");
-        label.setFont(new Font("Serif", Font.PLAIN, 20));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
 
-        this.add(comboBox, new GridBagConstraints(0, 0, 1, 1, 0, 0,
+        panelUp.add(comboBox, new GridBagConstraints(0, 0, 3, 1, 0, 0,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(20, 20, 20, 20), 0, 0));
-        this.add(checkBox, new GridBagConstraints(0, 1, 1, 1, 0, 0,
+                new Insets(20, 20, 10, 20), 0, 0));
+        panelUp.add(checkBox, new GridBagConstraints(0, 1, 1, 1, 0, 0,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(20, 20, 20, 20), 0, 0));
-        this.add(field, new GridBagConstraints(0, 2, 1, 1, 0, 0,
+                new Insets(20, 20, 10, 20), 0, 0));
+        panelUp.add(field, new GridBagConstraints(0, 2, 3, 1, 0, 0,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(20, 20, 20, 20), 0, 0));
-        this.add(button, new GridBagConstraints(0, 3, 1, 1, 0, 0,
+                new Insets(20, 20, 10, 20), 0, 0));
+        panelUp.add(button, new GridBagConstraints(0, 3, 1, 1, 0, 0,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(20, 20, 20, 20), 0, 0));
-        this.add(label, new GridBagConstraints(0, 4, 1, 1, 0, 0,
-                GridBagConstraints.NORTH, GridBagConstraints.NONE,
-                new Insets(20, 20, 20, 20), 0, 0));
+                new Insets(20, 20, 10, 20), 0, 0));
 
-        this.pack();
+        panelDown.add(label);
+
         this.setVisible(true);
-
-        System.out.println(comboBox.getItemAt(comboBox.getSelectedIndex()));
 
         button.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (checkBox.isSelected()) {
-                    label.setText("Ответ: " + field.getText());
-                } else {
-                    label.setText("Ответ: " + comboBox.getItemAt(comboBox.getSelectedIndex()));
-                }
+            if (checkBox.isSelected()) {
+                label.setText("Ответ: " + field.getText());
+            } else {
+                label.setText("Ответ: " + comboBox.getItemAt(comboBox.getSelectedIndex()));
+            }
             }
         });
 
         checkBox.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                field.setEnabled(((JCheckBox)e.getSource()).isSelected());
-            }
-        });
-
-        comboBox.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JComboBox cb = (JComboBox) e.getSource();
-                cb.getItemAt(cb.getSelectedIndex());
+            field.setEnabled(((JCheckBox)e.getSource()).isSelected());
             }
         });
     }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
